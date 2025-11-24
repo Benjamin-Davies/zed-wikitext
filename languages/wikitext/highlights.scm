3c8b05be0f8ef2df6c8ed29600aefc4a6e81ed52
Benjamin Davies <bentendavies@gmail.com>
2025-11-24 16:20:54 +1300

    feat: basic syntax highlighting

---
+++ languages/wikitext/highlights.scm
@@ -0,0 +1,93 @@
+(text) @text
+
+(heading1
+  (heading_marker) @title.markup.marker
+  (text) @title.markup.1
+  (heading_marker) @title.markup.marker
+)
+(heading2
+  (heading_marker) @title.markup.marker
+  (text) @title.markup.2
+  (heading_marker) @title.markup.marker
+)
+(heading3
+  (heading_marker) @title.markup.marker
+  (text) @title.markup.3
+  (heading_marker) @title.markup.marker
+)
+(heading4
+  (heading_marker) @title.markup.marker
+  (text) @title.markup.4
+  (heading_marker) @title.markup.marker
+)
+(heading5
+  (heading_marker) @title.markup.marker
+  (text) @title.markup.5
+  (heading_marker) @title.markup.marker
+)
+(heading6
+  (heading_marker) @title.markup.marker
+  (text) @title.markup.6
+  (heading_marker) @title.markup.marker
+)
+
+(wikilink
+  (wikilink_page) @link_uri.markup
+  (page_name_segment)? @link_text.markup
+)
+(external_link
+  (url) @link_uri.markup
+  (page_name_segment)? @link_text.markup
+)
+
+(template
+  (template_name) @function
+  (template_argument
+  (template_param_name)? @attribute
+  (template_param_value)? @string
+  )
+)
+
+(comment) @comment
+
+[
+  "[["
+  "]]"
+  "{{"
+  "}}"
+  "{|"
+  "|}"
+  "["
+  "]"
+  "<"
+  ">"
+  "</"
+] @punctuation.bracket
+
+[
+  "|"
+  "|-"
+  "|+"
+  "!"
+  "!!"
+  "||"
+] @punctuation.delimiter
+
+(table_header_block
+  (content) @emphasis.strong
+)
+(table_header_inline
+  (content) @emphasis.strong
+)
+
+(html_tag_name) @tag
+(html_attribute
+  (html_attribute_name) @attribute
+)
+(html_attribute
+  (html_attribute_name) @attribute
+  (html_attribute_value) @string
+)
+
+(italic) @emphasis
+(bold) @emphasis.strong
