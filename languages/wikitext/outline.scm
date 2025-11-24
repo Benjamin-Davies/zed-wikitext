d564f3cb18cd0a82a22e5137367cc471e07b059c
Benjamin Davies <bentendavies@gmail.com>
2025-11-24 17:05:10 +1300

    feat: other basic language features

---
+++ languages/wikitext/outline.scm
@@ -0,0 +1,30 @@
+(heading1
+  (heading_marker) @context
+  (text) @name
+  (heading_marker) @context
+) @item
+(heading2
+  (heading_marker) @context
+  (text) @name
+  (heading_marker) @context
+) @item
+(heading3
+  (heading_marker) @context
+  (text) @name
+  (heading_marker) @context
+) @item
+(heading4
+  (heading_marker) @context
+  (text) @name
+  (heading_marker) @context
+) @item
+(heading5
+  (heading_marker) @context
+  (text) @name
+  (heading_marker) @context
+) @item
+(heading6
+  (heading_marker) @context
+  (text) @name
+  (heading_marker) @context
+) @item
